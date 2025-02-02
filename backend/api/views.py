04b3808a929937a0eed02b86f7235f2ed096d551
Kyahwill <knownbutnotloved@gmail.com>
2025-02-02 19:32:13 +0800

    feat(frontend): initialized progressive web app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -17,10 +17,31 @@ class CoverageViewSet(viewsets.ModelViewSet):
     serializer_class = CoverageSerializer
     permission_classes = [permissions.AllowAny]
 
+    def get_queryset(self,):
+        exam_id = self.request.query_params.get('exam_id',None)
+        if exam_id is not None:
+            try:
+                exam = Exam.objects.get(pk=exam_id)
+                self.queryset = Coverage.objects.filter(exam=exam)
+            except Exam.DoesNotExist:
+                self.queryset = Coverage.objects.none()
+        return self.queryset
+
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
     permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self,):
+        coverage_id = self.request.query_params.get('coverage_id',None)
+        if coverage_id is not None:
+            try:
+                coverage = Coverage.objects.get(pk=coverage_id)
+                self.queryset = Subject.objects.filter(coverage=coverage)
+            except Exam.DoesNotExist:
+                self.queryset = Coverage.objects.none()
+        return self.queryset
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,43 @@
-<script>
+<script lang="ts">
 	import { goto } from "$app/navigation";
 	import { Button } from "$lib/components/ui/button";
+    import * as Select  from "$lib/components/ui/select";
 	//import type { PageProps } from './$types';
 
 	let { data } = $props();
-	console.log(data.exams)
-	const redirect = () => {
-		const course = localStorage.getItem("course");
+	const exams = data.exams
+    let value= $state('')
+    const triggerContent = $derived(
+        exams.find((f: any) => f.id === value)?.title ?? "Select Exams"
+    );
 
-		if(course !== null && course !== ""){
-			goto("/quiz");
-		}
-		goto("/configure");
-	}
-
+    const saveItem = () => {
+        if(value !== "") {
+            localStorage.setItem("exam",value)
+            goto("/home")
+        } else {
+            window.alert("Please select a valid exam")
+        }
+    }
 </script>
 <div class="mx-auto w-80 px-4 py-5 border-red-700 flex justify-center flex-col">
 	<h1 class="text-center font-semibold text-4xl mb-20" > Online Reviewer </h1>
-	<Button onclick={redirect}>
-		<p>Start Quiz</p>
-	</Button>
+        <Select.Root  type="single" name="favoriteFruit" bind:value >
+            <Select.Trigger >
+                {triggerContent}
+            </Select.Trigger>
+            <Select.Content>
+                <Select.Group> 
+                    <Select.GroupHeading>Courses</Select.GroupHeading>
+            {#each exams as exam} 
+                <Select.Item value={exam.id} label={exam.title}>
+                    {exam.title}
+                </Select.Item>
+            {/each}
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
+        <Button onclick={saveItem}>
+            Save Item
+        </Button>
 </div>
